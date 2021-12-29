76a931690f0d2a31abe65a1a4a40a86e3ef2948e
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2021-12-29 11:51:54 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,11 +5,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """accepts mixed arguments and return the square of v and str in tuple"""
-    return (k, v * v)#!/usr/bin/env python3
-"""Annotation of mixed function parameters"""
-from typing import Union, Tuple
-
-
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """accepts mixed arguments and return the square of v and str in tuple"""
     return (k, v * v)
