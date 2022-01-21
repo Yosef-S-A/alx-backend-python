a28942e004ecaeb9b37405986a845ca626a5e89a
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-21 13:34:00 -0500

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,11 @@ import random
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """ loop 10 times, asynchronously wait for 1 second, return random num between 1 and 10   
+    Returns
+    ------
+    random number between 1 and 10
+    """
     for i in range(10):
-        yield random.random()
+        yield random.random() * 10
         await asyncio.sleep(1)
