b25549cd72eb6490a426b80865b2d48ebd3652d7
Yosef Samuel <78106133+Yosef-S-A@users.noreply.github.com>
2022-01-21 12:09:08 -0500

    Update 3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Tasks """
 
 import asyncio
 import random
@@ -6,4 +7,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """ returns asyncio.Task object after waiting for random delay 
+    Parameters
+    ----------
+    max_delay : int
+        maximum delay in seconds
+        
+    Returns
+    -------
+        asyncio.Task 
+    """
     return asyncio.create_task(wait_random(max_delay))
